1eaa2722480a306320feccfbd60bc28bb0f9d1d4
trinhphong <nguyenquoctin19961996@gmail.com>
2017-04-26 18:43:40 +0700

    Multi Auth Login

---
+++ resources/views/client/home/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3" style="background-color: yellow">
+                <div class="nav">
+                    
+                </div>
+            </div>
+            <div class="col-md-9" style="background-color: red">
+
+            </div>
+        </div>
+    </div>
+@endsection
