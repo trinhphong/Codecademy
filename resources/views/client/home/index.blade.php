018a9a65fa2a4a3b0b5e0fa24d5c9b71e5dd07c7
trinhphong <nguyenquoctin19961996@gmail.com>
2017-04-26 20:58:12 +0700

    Homepage 1

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -3,13 +3,15 @@
 @section('content')
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-3" style="background-color: yellow">
-                <div class="nav">
-                    
+            <div class="col-md-offset-2 col-md-8" style="background-color: red">
+                <div class="col-md-12" style="background-color: #E9EAEA; padding: 0">
+                    <div class="col-md-3" style="background-color: #204056;padding: 0">
+                        <img src="{{asset('assets/images/icon/html5.png')}}" class="img-rounded">
+                    </div>
+                    <div class="col-md-9">
+                        <div class="panel-title">ABC</div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-md-9" style="background-color: red">
-
             </div>
         </div>
     </div>
